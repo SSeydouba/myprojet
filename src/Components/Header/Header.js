af71bde87e3a08b19a81eb74594c1231daf05561
SSoumah <seydouba.soumah1@gmail.com>
2023-12-30 19:34:30 +0100

    Modif6

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,17 @@ import mealIcon from "../../assets/meal-icon.png";
 import { Link } from 'react-router-dom';
 import './icons'; 
 import Search from '../Search/Search';
+import Nav from '../../Containers/Nav/Nav';
 
 // Composant Header
 const Header = () => {
   return (
     <div className='header-container'>   
       <h1>Welcome to TheMealDB</h1>
+      
+      {/* Inclusion Nav */}
+      <Nav />
+
       <div className='logo'>
         <Link to={"/"} alt="" className='logo-image'>
           <img src={mealIcon} alt='Meal icon'
---
+++ src/Containers/Nav/Nav.js
@@ -0,0 +1,20 @@
+import './nav.css';
+import { Link } from 'react-router-dom';
+
+
+const Nav = () =>{
+    return(
+        <div className='nav-container'>
+            <div className='content-home'>
+                <Link to={"/"} alt="" className='link-home'>
+                    <h3> Home</h3>
+                </Link>
+            </div>
+           
+                <h3>Inscription</h3>
+                <h3>Connexion</h3>
+           
+        </div>
+    )
+}
+export default Nav;
