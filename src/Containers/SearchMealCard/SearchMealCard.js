32c9855d04a7568f0d0385597e68483f428d0a10
SSoumah <seydouba.soumah1@gmail.com>
2024-01-01 12:38:22 +0100

    Modif7

--- src/Containers/SearchMealCard/SearchMealCard.js
+++ src/Containers/SearchMealCard/SearchMealCard.js
@@ -1,20 +1,22 @@
 import './searchMealCard.css';
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Composant SearchMealCard
 const SearchMealCard = ({ meals }) => {
+
   return (
     <div className="search-conatainer">
         <div className='img-meals'>
-          {/* <Link to={`/search/${encodeURIComponent(meals.id)}`} className="meals-details-link"> */}
-          <img
-            src={`${meals.strMealThumb}`}
-            alt={meals.strMeal}
-            className="cardImg"
-          />
+         <Link to={`/meal/${meals.strMeal}`} className="meals-details-link"> 
+            <img
+              src={`${meals.strMealThumb}`}
+              alt={meals.strMeal}
+              className="cardImg"
+            />
+          </Link> 
         </div>
-        {/* </Link> */}
+        
         <p className='title'>
           {meals.strMeal}
         </p>
